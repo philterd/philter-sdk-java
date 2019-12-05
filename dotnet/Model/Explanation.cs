5aa406a72acc9a6a650b0a3e58455988675d8fb4
jzonthemtn <jeff.zemerick@mtnfog.com>
2019-12-05 07:42:21 -0500

    Adding object classes.

---
+++ dotnet/Model/Explanation.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+
+namespace Philter.Model
+{
+    public class Explanation
+    {
+
+        private List<Span> _appliedSpans;
+        private List<Span> _identifiedSpans;
+
+        public List<Span> appliedSpans
+        {
+            get
+            {
+                return this._appliedSpans;
+            }
+            set
+            {
+                this._appliedSpans = value;
+            }
+        }
+
+        public List<Span> identifiedSpans
+        {
+            get
+            {
+                return this._identifiedSpans;
+            }
+            set
+            {
+                this._identifiedSpans = value;
+            }
+        }
+
+    }
+}
---
+++ dotnet/Model/Span.cs
@@ -0,0 +1,99 @@
+﻿namespace Philter.Model
+{
+    public class Span
+    { 
+
+        private int _characterStart;
+        private int _characterEnd;
+        private string _context;
+        private string _documentId;
+        private double _confidence;
+        private string _replacement;
+        private FilterType _filterType;
+
+        public int characterStart
+        {
+            get
+            {
+                return this._characterStart;
+            }
+            set
+            {
+                this._characterStart = value;
+            }
+        }
+
+        public int characterEnd
+        {
+            get
+            {
+                return this._characterEnd;
+            }
+            set
+            {
+                this._characterEnd = value;
+            }
+        }
+
+        public string context
+        {
+            get
+            {
+                return this._context;
+            }
+            set
+            {
+                this._context = value;
+            }
+        }
+
+        public string documentId
+        {
+            get
+            {
+                return this._documentId;
+            }
+            set
+            {
+                this._documentId = value;
+            }
+        }
+
+        public double confidence
+        {
+            get
+            {
+                return this._confidence;
+            }
+            set
+            {
+                this._confidence = value;
+            }
+        }
+
+        public string replacement
+        {
+            get
+            {
+                return this._replacement;
+            }
+            set
+            {
+                this._replacement = value;
+            }
+        }
+
+        public FilterType filterType
+        {
+            get
+            {
+                return this._filterType;
+            }
+            set
+            {
+                this._filterType = value;
+            }
+        }
+
+    }
+}
