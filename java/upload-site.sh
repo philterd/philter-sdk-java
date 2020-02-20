574f43a1a7e2caa8ea524b5720b701a84837ed8d
jzonthemtn <jeff.zemerick@mtnfog.com>
2020-02-20 14:21:16 -0500

    Setting pom name.

--- java/upload-site.sh
+++ java/upload-site.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-VERSION=`mvn -q -Dexec.executable="echo" -Dexec.args='${project.version}' --non-recursive exec:exec`
+VERSION=`mvn -f ./pom.xml -q -Dexec.executable="echo" -Dexec.args='${project.version}' --non-recursive exec:exec`
 aws s3 sync ./target/site/ s3://mtnfog-public/philter-sdk/java/$VERSION/
