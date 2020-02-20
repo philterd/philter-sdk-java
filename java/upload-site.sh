c65b4dd06605648fcfa104615a161254566cb8c0
jzonthemtn <jeff.zemerick@mtnfog.com>
2020-02-20 14:24:01 -0500

    Setting pom name.

--- java/upload-site.sh
+++ java/upload-site.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 WD=`pwd`
-VERSION=`mvn -f $WD/pom.xml -q -Dexec.executable="echo" -Dexec.args='${project.version}' --non-recursive exec:exec`
-aws s3 sync ./target/site/ s3://mtnfog-public/philter-sdk/java/$VERSION/
+VERSION=`mvn -f $WD/java/pom.xml -q -Dexec.executable="echo" -Dexec.args='${project.version}' --non-recursive exec:exec`
+aws s3 sync $WD/java/target/site/ s3://mtnfog-public/philter-sdk/java/$VERSION/
