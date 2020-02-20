dc539617702a16a80acd0e342d28d5d5f762bb83
jzonthemtn <jeff.zemerick@mtnfog.com>
2020-02-20 14:25:31 -0500

    Deleting nonexistent files.

--- java/upload-site.sh
+++ java/upload-site.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 WD=`pwd`
 VERSION=`mvn -f $WD/java/pom.xml -q -Dexec.executable="echo" -Dexec.args='${project.version}' --non-recursive exec:exec`
-aws s3 sync $WD/java/target/site/ s3://mtnfog-public/philter-sdk/java/$VERSION/
+aws s3 sync $WD/java/target/site/ s3://mtnfog-public/philter-sdk/java/$VERSION/ --delete
