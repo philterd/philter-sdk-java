46e890d26cb1082a5a7fd52b0a348ece02689d0b
jzonthemtn <jeff.zemerick@mtnfog.com>
2020-05-16 17:40:24 -0400

    Adding models service.

---
+++ src/main/java/com/mtnfog/philter/ModelsClient.java
@@ -0,0 +1,132 @@
+/*******************************************************************************
+ * Copyright 2020 Mountain Fog, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License.  You may obtain a copy
+ * of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ ******************************************************************************/
+package com.mtnfog.philter;
+
+import com.mtnfog.philter.model.Model;
+import com.mtnfog.philter.model.exceptions.ClientException;
+import com.mtnfog.philter.model.exceptions.ServiceUnavailableException;
+import com.mtnfog.philter.services.ModelsService;
+import okhttp3.ConnectionPool;
+import okhttp3.OkHttpClient;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+import retrofit2.converter.scalars.ScalarsConverterFactory;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+public class ModelsClient {
+
+	public static final String ENDPOINT = "https://www.mtnfog.com/api/models";
+
+	public static final int DEFAULT_TIMEOUT_SEC = 30;
+	public static final int DEFAULT_MAX_IDLE_CONNECTIONS = 20;
+	public static final int DEFAULT_KEEP_ALIVE_DURATION_MS = 30 * 1000;
+
+	private ModelsService service;
+
+	public static class ModelsClientBuilder {
+
+		private OkHttpClient.Builder okHttpClientBuilder;
+		private long timeout = DEFAULT_TIMEOUT_SEC;
+		private int maxIdleConnections = DEFAULT_MAX_IDLE_CONNECTIONS;
+		private int keepAliveDurationMs = DEFAULT_KEEP_ALIVE_DURATION_MS;
+
+		public ModelsClientBuilder withOkHttpClientBuilder(OkHttpClient.Builder okHttpClientBuilder) {
+			this.okHttpClientBuilder = okHttpClientBuilder;
+			return this;
+		}
+
+		public ModelsClientBuilder withTimeout(long timeout) {
+			this.timeout = timeout;
+			return this;
+		}
+
+		public ModelsClientBuilder withMaxIdleConnections(int maxIdleConnections) {
+			this.maxIdleConnections = maxIdleConnections;
+			return this;
+		}
+
+		public ModelsClientBuilder withKeepAliveDurationMs(int keepAliveDurationMs) {
+			this.keepAliveDurationMs = keepAliveDurationMs;
+			return this;
+		}
+
+		public ModelsClient build() {
+			return new ModelsClient(okHttpClientBuilder, timeout, maxIdleConnections, keepAliveDurationMs);
+		}
+
+	}
+
+	private ModelsClient(OkHttpClient.Builder okHttpClientBuilder, long timeout, int maxIdleConnections, int keepAliveDurationMs) {
+
+		if(okHttpClientBuilder == null) {
+
+			okHttpClientBuilder = new OkHttpClient.Builder()
+					.connectTimeout(timeout, TimeUnit.SECONDS)
+					.writeTimeout(timeout, TimeUnit.SECONDS)
+					.readTimeout(timeout, TimeUnit.SECONDS)
+					.connectionPool(new ConnectionPool(maxIdleConnections, keepAliveDurationMs, TimeUnit.MILLISECONDS));
+
+		}
+
+		final OkHttpClient okHttpClient = okHttpClientBuilder.build();
+
+		final Retrofit.Builder builder = new Retrofit.Builder()
+				.baseUrl(ENDPOINT)
+				.client(okHttpClient)
+				.addConverterFactory(ScalarsConverterFactory.create())
+				.addConverterFactory(GsonConverterFactory.create());
+
+		final Retrofit retrofit = builder.build();
+
+		service = retrofit.create(ModelsService.class);
+
+	}
+
+	private ModelsClient(OkHttpClient.Builder okHttpClientBuilder) {
+
+		this(okHttpClientBuilder, DEFAULT_TIMEOUT_SEC, DEFAULT_MAX_IDLE_CONNECTIONS, DEFAULT_KEEP_ALIVE_DURATION_MS);
+
+	}
+
+	public List<Model> getModels() throws IOException {
+
+		final Response<List<Model>> response = service.getModels().execute();
+
+		if(response.isSuccessful()) {
+
+			return response.body();
+
+		} else {
+
+			if(response.code() == 503) {
+
+				throw new ServiceUnavailableException("Service unavailable");
+
+			} else {
+
+				throw new ClientException("Unknown error: HTTP " + response.code());
+
+			}
+
+		}
+
+	}
+
+}
